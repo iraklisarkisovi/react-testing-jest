eef9b82a2de6b781002c142db7d7c74346744acd
iraklisarkisovi <sarqisovirakli@gmail.com>
2024-12-10 23:01:40 +0400

    Main

---
+++ src/App.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/edit/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/and save to reload./i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+ 
